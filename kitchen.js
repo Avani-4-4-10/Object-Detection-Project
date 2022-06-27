420e5ac85d64389c58b038fc29d4c94fd7d1802a
Avani-4-4-10 <92733721+Avani-4-4-10@users.noreply.github.com>
2022-06-27 19:00:58 +0530

    Add files via upload

---
+++ kitchen.js
@@ -0,0 +1,44 @@
+img=""
+status1=""
+objects= []
+
+function preload(){
+    img= loadImage("kitchen.jpg.jpg")
+}
+function setup(){
+    canvas= createCanvas(640 ,291)
+    canvas.center()
+    objectDetector = ml5.objectDetector( "cocossd" , modelLoaded)
+    document.getElementById("status").innerHTML= "Status = Detecting Object"
+
+}
+function modelLoaded(){
+    console.log("Model is initialized")
+    status1 = true
+    objectDetector.detect(img , gotResults)
+}
+function gotResults(error , results){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log(results)
+        objects = results
+    }
+}
+function draw(){
+    image(img , 0 , 0 , 640 , 291)
+    if(status1 != ""){
+        for(i = 0 ; i < objects.length ; i++){
+            document.getElementById("status").innerHTML= "Status = Object Detected "
+            fill("red")
+            percent = Math.floor(objects[i].confidence*100 )
+            text(objects[i].label + " " + percent + "%" , objects[i].x+15 , objects[i].y+15)
+            noFill()
+            stroke("red")
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height)
+        }
+    }
+
+
+}
